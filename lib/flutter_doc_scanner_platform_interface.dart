796baa99b5d757b55afeb6349f41f86fcf976479
SudaisRehman888 <sudaisrehman.zoobiapps@gmail.com>
2025-01-06 14:44:29 +0500

    Update flutter_doc_scanner_platform_interface.dart

--- lib/flutter_doc_scanner_platform_interface.dart
+++ lib/flutter_doc_scanner_platform_interface.dart
@@ -27,11 +27,11 @@ abstract class FlutterDocScannerPlatform extends PlatformInterface {
     throw UnimplementedError('platformVersion() has not been implemented.');
   }
 
-  Future<dynamic> getScanDocuments([int page = 5]) {
+  Future<dynamic> getScanDocuments([int page = 30]) {
     throw UnimplementedError('ScanDocuments() has not been implemented.');
   }
 
-  Future<dynamic> getScanDocumentsUri([int page = 5]) {
+  Future<dynamic> getScanDocumentsUri([int page = 30]) {
     throw UnimplementedError('ScanDocuments() has not been implemented.');
   }
 }
